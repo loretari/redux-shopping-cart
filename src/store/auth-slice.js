7828288922fe27251021478f9506af57b6349fab
loretari <loretari@gmail.com>
2023-08-28 10:54:17 +0300

    init: add cart-slice

--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -2,7 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
     name: "auth",
-    initialState: { isLoggedIn: false },
+    initialState: {
+        isLoggedIn: false
+    },
     reducers: {
         login(state) {
             state.isLoggedIn = true;
