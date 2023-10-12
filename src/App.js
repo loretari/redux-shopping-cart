b5aa49d6cd061f091e863dbe625f1b2f84d6a704
loretari <loretari@gmail.com>
2023-10-12 15:45:07 +0300

    init: add Notification

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import {useSelector} from "react-redux";
+import Notification from "./components/Notification";
 
 function App() {
     const cart = useSelector(state => state.cart);
@@ -11,13 +12,21 @@ function App() {
    //  const cartItems = useSelector((state) => state.cart.itemsList)
    //  console.log(cartItems)
     useEffect(() => {
-   fetch('https://redux-shopping--cart-default-rtdb.firebaseio.com/cartItems.json', {
-       method: "PUT",
-       body: JSON.stringify(cart)
-   })
-    }, [cart])
+        const sendRequest = async ()=>{
+            const res = await  fetch('https://redux-shopping--cart-default-rtdb.firebaseio.com/cartItems.json',
+                {
+                    method: "PUT",
+                    body: JSON.stringify(cart),
+                }
+            );
+            const data = await res.json();
+        }
+
+sendRequest();
+    }, [cart]);
     return (
         <div className="App">
+            <Notification type= 'success' message={'This is dummy message'}/>
            {!isLoggedIn && <Auth />}
             {isLoggedIn && <Layout />}
 
