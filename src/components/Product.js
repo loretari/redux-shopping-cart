2e8caf346ba57e60e67d3f95a38a10e538a95023
loretari <loretari@gmail.com>
2023-10-12 12:03:53 +0300

    init: add Cart-Item

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import "./Product.css";
 import {cartActions} from "../store/cart-slice";
 
