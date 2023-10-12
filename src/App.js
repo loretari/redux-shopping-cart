18f8a9ad59c986f340505aada34fcc4b9d25c1f7
loretari <loretari@gmail.com>
2023-10-12 16:44:01 +0300

    init: add ui-slice

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import {useDispatch, useSelector} from "react-redux";
 import Notification from "./components/Notification";
-import {uiActions} from "./store/ui-slice";
 import {sendCartData} from "./store/cart-slice";
 
 
